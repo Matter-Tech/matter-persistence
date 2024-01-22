983ee40e4045fbf2c97d255c625068f15631c2af
Rômulo Jales <romulo@thisismatter.com>
2024-01-22 15:00:35 +0100

    removing pydantic as dependency

--- matter_persistence/database/config.py
+++ matter_persistence/database/config.py
@@ -1,7 +1,6 @@
-from typing import Optional, List
+from dataclasses import dataclass
 from urllib.parse import quote
 
-from pydantic import SecretStr, BaseModel
 
 from .exceptions import InvalidDatabaseConfigurationError
 
@@ -29,7 +28,7 @@ def build_connection_uri(engine, username, password, host, port, dbname):
 
     return (
         f"{engine}://"
-        f"{url_encode(username.get_secret_value())}:{url_encode(password.get_secret_value())}"
+        f"{url_encode(username)}:{url_encode(password)}"
         f"@{url_encode(host)}:{url_encode(str(port))}"
         f"/{url_encode(dbname)}"
     )
@@ -37,10 +36,10 @@ def build_connection_uri(engine, username, password, host, port, dbname):
 
 FILE_NAME_TEMPLATE = "%%(year)d%%(month).2d%%(day).2d-%%(hour).2d%%(minute).2d%%(second).2d-%%(slug)s"
 
-
-class DatabaseMigrationConfig(BaseModel):
+@dataclass
+class DatabaseMigrationConfig:
     path: str
-    models: List[str]
+    models: list[str]
     file_template: str = FILE_NAME_TEMPLATE
     version_schema: str | None = None
 
@@ -48,16 +47,16 @@ class DatabaseMigrationConfig(BaseModel):
 class DatabaseConfig:
     def __init__(
         self,
-        engine: Optional[str] = None,
-        username: Optional[SecretStr] = None,
-        password: Optional[SecretStr] = None,
-        host: Optional[str] = None,
-        port: Optional[int] = None,
-        dbname: Optional[str] = None,
-        pool_size: Optional[int] = None,
-        connection_uri: Optional[str] = None,
+        engine: str | None = None,
+        username: str | None = None,
+        password: str | None  = None,
+        host: str | None  = None,
+        port: int| None  = None,
+        dbname: str  | None  = None,
+        pool_size: int | None  = None,
+        connection_uri: str | None = None,
         migration: DatabaseMigrationConfig | dict | None = None,
-        models: List[str] | None = None,
+        models: list[str] | None = None,
     ):
         self.pool_size = pool_size
         self.migration = DatabaseMigrationConfig(**migration) if migration else None
