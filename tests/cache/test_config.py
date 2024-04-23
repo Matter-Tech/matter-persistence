7ea8b17457b642c88d3d52107b7b232090f573f2
András Nagy <andras@thisismatter.com>
2024-04-23 12:56:12 +0200

    remove old modules

--- tests/cache/test_config.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from matter_persistence.cache import CacheEngine, CacheConfig
-from matter_persistence.cache import InvalidCacheConfigurationError
-
-
-def test_config_throws_exception_when_engine_is_not_memory_and_no_endpoint_is_set():
-    with pytest.raises(InvalidCacheConfigurationError):
-        CacheConfig(engine=CacheEngine.MEMCACHED)
--- tests/database/migrations/conftest.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy as sa
-
-
-async def has_table_been_created(conn, table_name, expected: bool):
-    stmt = sa.text("SELECT EXISTS(SELECT * FROM sqlite_master WHERE type= :table AND name= :table_name)")
-    stmt = stmt.bindparams(table="table", table_name=table_name)
-
-    result = await conn.execute(stmt)
-    assert bool(result.scalar()) is expected
