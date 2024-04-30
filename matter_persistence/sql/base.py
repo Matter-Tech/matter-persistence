4817dfa1f3b79152f3b59b1ea8542387a1ceae00
András Nagy <andras@thisismatter.com>
2024-04-30 11:40:47 +0200

    use custom Timestamp until sqlalchemy_utils fixes deprecated "datetime.utcnow "usage

--- matter_persistence/sql/base.py
+++ matter_persistence/sql/base.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 from typing import Optional
 from uuid import UUID, uuid4
 
+import sqlalchemy as sa
 from pydantic import BaseModel
 from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy_utils.models import Timestamp
 
 
 class FilterOperators(Enum):
@@ -29,6 +29,45 @@ class Base(DeclarativeBase):
     """
 
     pass
+
+
+# this callable is used as the default value in Timestamp below
+def datetime_with_utc_tz() -> datetime:
+    return datetime.now(timezone.utc)  # noqa: UP017
+
+
+# this is copied from sqlalchemy_utils.models.Timestamp
+# datetime.utcnow is deprecated, so, until it is fixed, this copy is to be used
+# it makes sense to keep sqlalchemy_utils as dependency, for example to use @generic_repr
+class Timestamp:
+    """Adds `created` and `updated` columns to a derived declarative model.
+
+    The `created` column is handled through a default and the `updated`
+    column is handled through a `before_update` event that propagates
+    for all derived declarative models.
+
+    ::
+
+
+        import sqlalchemy as sa
+        from sqlalchemy_utils import Timestamp
+
+
+        class SomeModel(Base, Timestamp):
+            __tablename__ = 'somemodel'
+            id = sa.Column(sa.Integer, primary_key=True)
+    """
+
+    created = sa.Column(sa.DateTime(timezone=True), default=datetime_with_utc_tz, nullable=False)
+    updated = sa.Column(sa.DateTime(timezone=True), default=datetime_with_utc_tz, nullable=False)
+
+
+# this is copied from sqlalchemy_utils too
+@sa.event.listens_for(Timestamp, "before_update", propagate=True)
+def timestamp_before_update(mapper, connection, target):
+    # When a model with a timestamp is updated; force update the updated
+    # timestamp.
+    target.updated = datetime.now(tz=timezone.utc)  # noqa: UP017
 
 
 class CustomBase(Base, Timestamp):
