2fcccceb32ec184ce28e0979913175ca4c72357e
lukasz-matter <lukasz@thisismatter.com>
2024-06-25 15:40:47 +0200

    Add option of specifying cache credentials

--- matter_persistence/redis/utils.py
+++ matter_persistence/redis/utils.py
@@ -7,7 +7,7 @@ from redis import asyncio as aioredis
 
 
 @lru_cache(maxsize=1)
-def get_connection_pool(host: str, port: int, db: int = 0) -> aioredis.ConnectionPool:
+def get_connection_pool(host: str, port: int, db: int = 0, username: str = "default", password: str | None = None) -> aioredis.ConnectionPool:
     """
     Gets a connection pool to Redis. Note that it's a singleton - an application process should always only
     create 1 connection pool & reuse it for all internal connections.
@@ -16,12 +16,17 @@ def get_connection_pool(host: str, port: int, db: int = 0) -> aioredis.Connectio
         host (str): Redis host
         port (int): Redis port
         db (int): Redis logical database, normally only 0 is used
+        username (str): Redis username, defaults to "default", as mentioned in https://redis.io/docs/latest/commands/auth/
+        password (str): Redis password, optional
 
     Returns:
         connection_pool (aioredis.ConnectionPool): a connection pool to be reused by connections
             established in the application
     """
-    return aioredis.ConnectionPool.from_url(f"redis://{host}:{port}/{db}")
+    redis_url = f"redis://{host}:{port}/{db}"
+    if password is not None:
+        redis_url = redis_url.replace("://", f"://{username}:{password}@")
+    return aioredis.ConnectionPool.from_url(redis_url)
 
 
 def compress_pickle_data(data: Any) -> bytes:
